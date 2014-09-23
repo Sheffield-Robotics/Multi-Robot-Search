8c37370d8030bb29b8c4a7927cc43ba9ccb1f253
Andreas Kolling <andreas.kolling@gmail.com>
2014-09-23 18:53:07 +0100

    backup

--- modules/graphclear/src/surveillance_graph.cpp
+++ modules/graphclear/src/surveillance_graph.cpp
@@ -2,8 +2,6 @@
 
 using namespace graphclear;
 
-
-
 void surveillance_graph_t::cut_strategy()
 {
     std::deque< surveillance_graph_t::vertex_descriptor > q;
@@ -12,9 +10,12 @@ void surveillance_graph_t::cut_strategy()
     surveillance_graph_t::vertex_iterator vi, vi_end;
     for (boost::tie(vi, vi_end) = vertices(*this); vi != vi_end; ++vi)
     {
+        std::cout << " " << out_degree(*vi,*this) << " " 
+            << degree(*vi,*this) << std::endl;
         if ( out_degree(*vi,*this) == 1 ) 
             q.push_back(*vi);
     }
+    std::cout << " Leaves pushed into queue " << q.size() << std::endl;
     
     while ( ! q.empty() )
     {
@@ -81,19 +82,18 @@ surveillance_graph_t::construct_full_cut_sequence(
     boost::tie(ei, ei_end) = out_edges(to,*this);
     for ( ; ei != ei_end; ++ei ) 
     {
-        v_x = this->get_other(ei,v_y);
-        if ( get_cut_sequence(v_y,v_x).empty() == true ) {
+        v_x = this->get_other(ei,to);
+        if ( get_cut_sequence(to,v_x).empty() == true ) {
 
         } else {
             
-            graphclear::cut_sequence_t c = get_cut_sequence(v_y,v_x);
+            graphclear::cut_sequence_t c = get_cut_sequence(to,v_x);         
             
             
         }
     }
     
     
-    return s;
 }
 
 int surveillance_graph_t::count_outgoing_sequences()
@@ -119,7 +119,29 @@ int main (int argc, char const *argv[])
         surveillance_graph_t> SWGen;
     boost::minstd_rand gen;
     // Create graph with 100 nodes 
+    
     surveillance_graph_t g(SWGen(gen, 100, 6, 0.03), SWGen(), 100);    
     
+    std::cout << num_vertices(g) << " vertices " << std::endl;
+    std::cout << num_edges(g) << " edges " << std::endl;
+    
+    surveillance_graph_t::vertex_iterator v_i,v_end;
+    tie(v_i,v_end) = vertices(g);
+    for ( ; v_i != v_end; ++v_i ) {
+        g[*v_i].w = rand() % 20;
+    }
+    
+    surveillance_graph_t::edge_iterator e_i,e_end;
+    tie(e_i,e_end) = edges(g);
+    for ( ; e_i != e_end; ++e_i ) {
+        g[*e_i].w = rand() % 10;
+    }
+    
+    std::vector < surveillance_graph_t::edge_descriptor > spanning_tree;
+    kruskal_minimum_spanning_tree(g, std::back_inserter(spanning_tree));
+    
+    
+    g.cut_strategy();
+        
     return 0;
 }
--- modules/include/graphclear/surveillance_graph.h
+++ modules/include/graphclear/surveillance_graph.h
@@ -5,13 +5,13 @@
 #include <boost/graph/random.hpp>
 #include <boost/graph/kruskal_min_spanning_tree.hpp>
 #include <boost/graph/connected_components.hpp>
+#include <boost/graph/small_world_generator.hpp>
+#include <boost/random/linear_congruential.hpp>
 
 #include "graphclear/cut_sequence.h"
 
 namespace graphclear
 {
-
- 
 
 class surveillance_graph_vertex
 {
@@ -27,6 +27,7 @@ public:
     cut_sequence_t cut_sequence_source_to_target;
     cut_sequence_t cut_sequence_target_to_source;
     int w;
+    int edge_weight;
 };
 
 typedef boost::adjacency_list
@@ -38,6 +39,16 @@ class surveillance_graph_t : public sg_base
 {
 public:
     surveillance_graph_t();
+    
+    template<class EdgeIterator>
+    surveillance_graph_t(EdgeIterator first, EdgeIterator last,
+                          vertices_size_type n)
+            : sg_base(first, last, n)    // Call the superclass constructor in the subclass' initialization list.
+            {
+                // do something with bar
+            };
+    
+    
     void cut_strategy();
     
     void
